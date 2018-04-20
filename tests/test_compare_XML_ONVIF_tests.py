5db12fcb6812a56c28bfc24439ef3fc6992ddf35
Marvin VAUGE <marvin.vauge@smardtv.com>
2018-04-20 16:37:14 +0200

    Tests construct_steps()

--- tests/test_compare_XML_ONVIF_tests.py
+++ tests/test_compare_XML_ONVIF_tests.py
@@ -9,6 +9,24 @@ class Test:
     def test_construct_steps_empty(self):
         assert len(construct_steps([])) == 0
     
-    def test_construct_steps(self):
-        pass
+    def test_construct_steps_same_number(self):
+        step_nodes = etree.parse('test_files\\steps.xml').findall('StepResult')
+        steps = construct_steps(step_nodes)
+        
+        assert len(steps) == 4
+    
+    def test_construct_steps_right_names(self):
+        step_nodes = etree.parse('test_files\\steps.xml').findall('StepResult')
+        steps = construct_steps(step_nodes)
+        
+        for i in range(len(steps)):
+            assert steps[i].name == step_nodes[i].find('StepName').text
+    
+    def test_construct_steps_right_results(self):
+        step_nodes = etree.parse('test_files\\steps.xml').findall('StepResult')
+        steps = construct_steps(step_nodes)
+        
+        for i in range(len(steps)):
+            assert steps[i].result == step_nodes[i].find('Status').text
+    
     
